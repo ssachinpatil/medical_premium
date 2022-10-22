ee0105640949c5ff56027085d044cf4f67490918
ssachinpatil <sachinsukase3a@gmail.com>
2022-10-22 10:22:59 -0800

    config added

---
+++ config.py
@@ -0,0 +1,8 @@
+
+import os
+# import json
+
+PORT_NUMBER=8080
+
+MODEL_FILE_PATH = os.path.join(os.getcwd(),r"project_app\Medical.pkl")
+JSON_FILE_PATH = os.path.join(os.getcwd(),r"project_app\data.json")
